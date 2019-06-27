304d097882c90739231bf6e95a4667c41c443bcd
evanfeng <evanfeng@pku.edu.cn>
2019-06-27 21:15:15 +0000

    Add PCNNEncoder PCNNClassifier

--- semparse/model.py
+++ semparse/model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
+from math import floor, ceil
 from layers import *
 
 
@@ -26,6 +28,59 @@ class CNNEncoder(nn.Module):
         outputs = [F.relu(conv(embeds)).squeeze(3) for conv in self.convs]  # batch_size, kernel_num, seq_len - C
         outputs = [F.max_pool1d(x, x.size(2)).squeeze(2) for x in outputs]
         outputs = torch.cat(outputs, 1)
+        return outputs
+
+
+class PCNNEncoder(nn.Module):
+    init_range = 0.1
+
+    def __init__(self, vocab_size, emb_size, kernel_num, kernel_sizes, embed_p, token_ids):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.emb_size = emb_size
+        self.kernel_num = kernel_num
+        self.kernel_sizes = kernel_sizes
+        self.embed_p = embed_p
+        self.token_ids = token_ids  # the IDs of the special tokens used to identify the locations of entities
+
+        self.emb = nn.Embedding(vocab_size, emb_size)
+        self.emb.weight.data.uniform_(-self.init_range, self.init_range)
+        self.emb_dp = EmbeddingDropout(self.emb, embed_p)
+        self.convs = nn.ModuleList([nn.Conv2d(1, kernel_num, (K, emb_size)) for K in kernel_sizes])
+
+    def forward(self, inputs):
+        """
+        inputs: torch.Tensor of shape (B, L)
+        inputs: torch.Tensor of shape (B, L)
+        """
+        dev = inputs.device
+        bs, sl = inputs.size()
+        inputs_np = inputs.cpu().numpy()
+        embeds = self.emb_dp(inputs)
+        embeds = embeds.unsqueeze(1)  # batch_size, 1, seq_len, emb_size
+        outputs = [conv(F.pad(embeds, [0, 0, floor((k - 1) / 2), ceil((k - 1) / 2)], value=0)).squeeze(3)
+                   for conv, k in zip(self.convs, self.kernel_sizes)]  # (B, C, L)
+        # outputs = [conv(embeds).squeeze(3) for conv in self.convs[:1]]
+        outputs = [F.relu(x) for x in outputs]
+        # outputs = [F.pad(x, (floor((k - 1) / 2), ceil((k - 1) / 2)), value=-1) for x, k in zip(outputs, self.kernel_sizes)]  # (B, C, L)
+        outputs = torch.cat(outputs, 1)  # (B, C', L)
+
+        pooled = []
+        ai_np = np.arange(bs)
+        ri = torch.arange(sl).to(dev).unsqueeze(0).expand(bs, sl)
+        prev_ids = inputs.new_zeros(bs)
+        # print(inputs == self.token_ids[0])
+        # print(inputs == self.token_ids[-1])
+        for t in self.token_ids:
+            ids_np = (inputs_np == t).argmax(1)
+            ids = torch.tensor(ids_np, device=dev)
+            inputs_np[ai_np, ids_np] = -1
+            lmask = (ri < prev_ids.unsqueeze(-1)).unsqueeze(1)
+            rmask = (ri > ids.unsqueeze(-1)).unsqueeze(1)
+            masked = outputs.masked_fill(lmask, float('-inf')).masked_fill(rmask, float('-inf'))
+            pooled.append(F.max_pool1d(masked, masked.size(2)).squeeze(2))  # (B, C)
+            prev_ids = ids
+        outputs = torch.cat(pooled, -1)
         return outputs
 
 
@@ -73,6 +128,29 @@ class CNNClassifier(nn.Module):
 
         self.fc_in_dim = kernel_num * len(kernel_sizes)
         self.encoder = CNNEncoder(vocab_size, emb_size, kernel_num, kernel_sizes, embed_p)
+        self.mlp = MLP(self.fc_in_dim, hidden_size, output_size, num_fc_layers, fc_p)
+
+    def forward(self, inputs):
+        return self.mlp(self.encoder(inputs))
+
+
+class PCNNClassifier(nn.Module):
+
+    def __init__(self, vocab_size, emb_size, kernel_num, kernel_sizes, embed_p,
+                 num_fc_layers, hidden_size, output_size, fc_p, tok_ids):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.emb_size = emb_size
+        self.kernel_num = kernel_num
+        self.kernel_sizes = kernel_sizes
+        self.embed_p = embed_p
+        self.num_fc_layers = num_fc_layers
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.fc_p = fc_p
+
+        self.fc_in_dim = len(tok_ids) * kernel_num * len(kernel_sizes)
+        self.encoder = PCNNEncoder(vocab_size, emb_size, kernel_num, kernel_sizes, embed_p, tok_ids)
         self.mlp = MLP(self.fc_in_dim, hidden_size, output_size, num_fc_layers, fc_p)
 
     def forward(self, inputs):
@@ -137,8 +215,8 @@ class ChainedCNNMultiLabelClassifier(nn.Module):
         history = []
         outputs = []
         for mlp in self.mlps:
-            intputs = torch.cat([encoded] + history, -1)
-            out = mlp(intputs)
+            inputs = torch.cat([encoded] + history, -1)
+            out = mlp(inputs)
             outputs.append(out)
             _, pred = out.max(-1)
             pred = pred.detach()
